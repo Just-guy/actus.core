18211fa2dc60b69afbb920d67ba0bd04715b3427
Andrej Belyi <Andrej.Belyj@itb-dev.ru>
2026-09-25 16:36:03 +0300

    Lang file update

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$MESS['ACTUS_CORE_MODULE_NAME'] = 'Actus: ядро';
+$MESS['ACTUS_CORE_MODULE_NAME'] = 'Actus: Ядро';
 $MESS['ACTUS_CORE_MODULE_DESC'] = 'Общая инфраструктура модулей Actus';
 $MESS['ACTUS_CORE_PARTNER_NAME'] = 'Actus';
 $MESS['ACTUS_CORE_PARTNER_URI'] = 'https://actus.dev';
